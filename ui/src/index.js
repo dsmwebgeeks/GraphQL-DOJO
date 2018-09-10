bf85f834dba401b8940abd72259094ce6172a702
Justin Stevens <justin@interactiveshift.com>
2018-09-10 16:58:36 -0500

    remove finished code

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql"
-});
 
 const Root = () => (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <App />
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
