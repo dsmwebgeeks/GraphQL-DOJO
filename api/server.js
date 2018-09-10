9c82d11670605ad9f91a4f9c0d58630af68d7d09
Justin Stevens <justin@interactiveshift.com>
2018-09-10 15:54:18 -0500

    changed port

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { makeExecutableSchema } = require('graphql-tools')
-const port = 9000
+const port = 3000
 const app = express()
 
 const typeDefs = `
